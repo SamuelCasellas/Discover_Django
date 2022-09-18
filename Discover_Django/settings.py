789b120850f58d92dc41ddd7d4f2fef6ab61e27a
Samuel Casellas <sam.casellas@gmail.com>
2022-09-18 15:24:48 -0600

    Added secret key via environ package

--- Discover_Django/settings.py
+++ Discover_Django/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
-from Discover_Django.secrets import SEC_KEY
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SEC_KEY
+# I replaced the secret key line with this
+SECRET_KEY = os.environ['SEC_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
